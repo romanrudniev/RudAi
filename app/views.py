3357b3841e8187869c2b16fee1c6737062be4a2e
romanrudniev <romanrudniev007@gmail/com>
2025-04-26 11:37:24 +0300

    add design

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.shortcuts import render
+from .forms import PromptFor
+
+def home(request):
+    response_text = None
+    image_url = None
+
+    if request.method == 'POST':
+        form = PromptFor(request.POST)
+        if form.is_valid():
+            prompt = form.cleaned_data['prompt']
+
+            # Поки що просто виводимо, що написав користувач
+            response_text = f"Ви написали: {prompt}"
+
+    else:
+        form = PromptFor()
+
+    return render(request, 'base.html', {
+        'form': form,
+        'response_text': response_text,
+        'image_url': image_url,
+    })
