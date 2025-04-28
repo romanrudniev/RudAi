710fe85842e58ae1e238c6ba1d1c0e23990108fb
romanrudniev <romanrudniev007@gmail/com>
2025-04-28 09:54:01 +0300

    add RegisterForm and LoginForm

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,29 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
 
 class PromptForm(forms.Form):
     prompt = forms.CharField(label="Enter your prompt", max_length=1000, widget=forms.Textarea)
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Паролі не співпадають!")
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label="Ім'я користувача")
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from .forms import PromptForm
+from django.shortcuts import render, redirect
+from .forms import PromptForm, RegisterForm, LoginForm
+from django.contrib.auth import authenticate, login
 import openai
-openai.api_key = ''
-client = openai.OpenAI()
+# openai.api_key = ''
+# client = openai.OpenAI()
 
 def home(request):
     response_text = None
@@ -13,17 +14,17 @@ def home(request):
         if form.is_valid():
             prompt = form.cleaned_data['prompt']
 
-            completion = client.chat.completions.create(
-                model="gpt-4.1",
-                messages=[
-                    {"role": "developer", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": prompt}
-                ]
-            )
+            # completion = client.chat.completions.create(
+            #     model="gpt-4.1",
+            #     messages=[
+            #         {"role": "developer", "content": "You are a helpful assistant."},
+            #         {"role": "user", "content": prompt}
+            #     ]
+            # )
+            #
+            # response_text = completion.choices[0].message
 
-            response_text = completion.choices[0].message
-
-            # response_text = f"Ви написали: {prompt}"
+            response_text = f"Ви написали: {prompt}"
 
     else:
         form = PromptForm()
@@ -33,3 +34,30 @@ def home(request):
         'response_text': response_text,
         'response_image': response_image,
     })
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
